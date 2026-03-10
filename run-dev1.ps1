481b1d1cce6615eafee9b4cd3c220d7cd3c98bb4
Jaewoo Cho <your-email@example.com>
2026-03-11 00:13:33 +0900

    .\run-dev1.ps1 경로 변경 및 업데이트

--- run-dev1.ps1
+++ run-dev1.ps1
@@ -1,4 +1,4 @@
-$projectRoot = "D:\SoundTools\sound-ai-tool"
+$projectRoot = "C:\Users\WD\Desktop\sound-ai-tool"
 
 Start-Process powershell -ArgumentList "-NoExit", "-Command", "Set-Location '$projectRoot\backend'; .\venv\Scripts\Activate.ps1; python -m uvicorn main:app --reload"
 
